7c584e837405df9fab2ac80dd9ddea507bff4e98
swayakrishna <165483828+swayakrishna@users.noreply.github.com>
2024-11-19 23:53:10 +0000

    Added models.py

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+#homepage route
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#adding new recipe route
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('add_recipe.html')
 
+#editing existing app route
 @app.route('/edit_recipe')
 def edit_recipe():
     return render_template('edit_recipe.html')
---
+++ models.py
@@ -0,0 +1,4 @@
+#This file contains all the models that is used in the application
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
