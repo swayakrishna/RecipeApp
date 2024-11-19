b3482c35f1cb788ece5ef94f92597bd8f7c85a1f
swayakrishna <165483828+swayakrishna@users.noreply.github.com>
2024-11-19 21:53:49 +0000

    Connecting add_recipe template

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
