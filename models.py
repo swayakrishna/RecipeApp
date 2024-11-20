97b9aed82e052e06681d783ee4d98d4ed81a2d39
swayakrishna <165483828+swayakrishna@users.noreply.github.com>
2024-11-20 00:08:01 +0000

    Added Recipe model

--- models.py
+++ models.py
@@ -2,3 +2,11 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    steps = db.Column(db.Text, nullable=False)
+    difficulty = db.Column(db.String(50), nullable=False)
+    cooking_time = db.Column(db.Integer, nullable=False)
