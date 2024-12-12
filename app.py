33312f541b8c42d0bb3b864d2d0ed90e37f50b66
swayakrishna <165483828+swayakrishna@users.noreply.github.com>
2024-12-12 07:14:34 +0000

    authenticating user

--- app.py
+++ app.py
@@ -12,11 +12,16 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+authenticated_user = None
+
 #homepage route
 @app.route('/')
 def index():
-    recipes = Recipe.query.all()  # Fetch all recipes from the database
-    return render_template('index.html', recipes=recipes)
+    if authenticated_user:
+        recipes = Recipe.query.all()  # Fetch all recipes from the database
+        return render_template('index.html', recipes=recipes)
+    else:
+        return redirect('/login')
 
 #adding new recipe route
 @app.route('/add_recipe', methods=['GET', 'POST'])
@@ -63,6 +68,49 @@ def delete_recipe(recipe_id):
     db.session.commit()
     flash('Recipe deleted successfully.', 'success')
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_pass = request.form['confirm_password']
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not email or not username or not password or not confirm_pass:
+            flash('All fields are required.', 'error')
+            return redirect(request.url)
+        if password != confirm_pass:
+            flash('Passwords doesnt match', 'error')
+        if existing_user:
+            flash('Username already exists. Please choose a different one.', 'error')
+        else:
+            new_user = User(username=username, password=password, email=email)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful. You can now login.', 'success')
+            return redirect('/login')
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global authenticated_user
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if not username or not password:
+            flash('Both username and password are required.', 'error')
+            return redirect(request.url)
+            
+        user = User.query.filter_by(username=username).first_or_404()
+
+        if user:
+            authenticated_user = username
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password.', 'error')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
--- models.py
+++ models.py
@@ -10,3 +10,15 @@ class Recipe(db.Model):
     steps = db.Column(db.Text, nullable=False)
     difficulty = db.Column(db.String(50), nullable=False)
     cooking_time = db.Column(db.Integer, nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
